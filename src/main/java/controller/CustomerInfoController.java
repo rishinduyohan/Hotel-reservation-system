ac6fc5a39185ba47a6e2df0a1528134e14ec7a79
Rishindu Yohan <rishinduyohan@gmail.com>
2025-10-07 14:25:46 +0530

    Create customer_info controller

--- src/main/java/controller/CustomerInfoController.java
+++ src/main/java/controller/CustomerInfoController.java
@@ -1,4 +1,63 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerInfoController {
+    @FXML
+    private TableColumn<?, ?> colAge;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colTelno;
+
+    @FXML
+    private TableView<?> tblCustomerInfo;
+
+    @FXML
+    private TextField txtAge;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCusId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtTelno;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
