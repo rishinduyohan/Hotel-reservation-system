995ec9fd2d328e77cf7bb413c3a793bd367eefea
Rishindu Yohan <rishinduyohan@gmail.com>
2025-10-08 20:54:58 +0530

    Add current window closing when new window is opened

--- src/main/java/controller/DashboardController.java
+++ src/main/java/controller/DashboardController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -43,6 +44,9 @@ public class DashboardController {
     public void btnSignOutOnAction(ActionEvent actionEvent) {
         try {
             stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Login_form.fxml"))));
+            Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            currentStage.close();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -2,6 +2,7 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
@@ -39,6 +40,8 @@ public class LoginFormController extends Component {
             cleanText();
             try {
                 stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Dashboard.fxml"))));
+                Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                currentStage.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
